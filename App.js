8676e32a8017d2c95a4e8f56a37edcd62e1486fa
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-23 22:21:55 +0530

    add SafeAreaView for iPhone's top notch

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+/**
+ * * for IOS -> UIView
+ * * for Android -> AndroidView
+*/
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Hello! React-Native</Text>
-    </View>
+    // SafeAreaView support for new iPhone's top notch 
+    <SafeAreaView style={styles.container}>
+      <Text>Hello! React-Native!</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue'
   },
 });
