aa406b4450069a7f3744095c3f95c6b7ffbf0bca
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-24 13:08:42 +0530

    add Alert component

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   SafeAreaView,
   Button,
+  Alert
 } from 'react-native';
 
 export default function App() {
@@ -10,10 +11,25 @@ export default function App() {
   return (
     // SafeAreaView support for new iPhone's top notch 
     <SafeAreaView style={styles.container}>
+      {/* alert method work on both android and IOS */}
       <Button
         title="Click Me"
         color="orange"
-        onPress={() => console.log('Button tapped')} />
+        onPress={() => Alert.alert('My title', 'My Message', [
+          {
+            text: 'Yes',
+            onPress: () => console.log('Yes')
+          },
+          {
+            text: 'No'
+          }
+        ])} />
+
+      {/* prompt method work on only IOS */}
+      <Button
+        title="Click Me"
+        color="purple"
+        onPress={() => Alert.prompt('My title', 'My Message', (text) => console.log(text))} />
     </SafeAreaView>
   );
 }
