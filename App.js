a4999fd9213a6864c90c751348245caa47d4ae0c
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-24 12:59:03 +0530

    add Button component

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import {
   StyleSheet,
-  Text,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  Image,
   SafeAreaView,
-  View
+  Button,
 } from 'react-native';
 
 export default function App() {
@@ -16,51 +10,10 @@ export default function App() {
   return (
     // SafeAreaView support for new iPhone's top notch 
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native</Text>
-      {/* TouchableWithoutFeedback do not give feedback to user */}
-      <TouchableWithoutFeedback onPress={() => console.log('TouchableWithoutFeedback')}>
-        <Image
-          fadeDuration={1000}
-          blurRadius={1}
-          source={{
-            uri: 'https://picsum.photos/350/200',
-            width: 350,
-            height: 200
-          }} />
-      </TouchableWithoutFeedback>
-
-      {/* TouchableOpacity gives a small opacity when pressed */}
-      <TouchableOpacity onPress={() => console.log('TouchableOpacity')}>
-        <Image
-          fadeDuration={1000}
-          blurRadius={1}
-          source={{
-            uri: 'https://picsum.photos/350/200',
-            width: 350,
-            height: 200
-          }} />
-      </TouchableOpacity>
-
-      {/* TouchableHighlight gives small feedback */}
-      <TouchableHighlight onPress={() => console.log('TouchableHighlight')}>
-        <Image
-          fadeDuration={1000}
-          blurRadius={1}
-          source={{
-            uri: 'https://picsum.photos/350/200',
-            width: 350,
-            height: 200
-          }} />
-      </TouchableHighlight>
-
-      {/* this gives native feedback only andriod */}
-      <TouchableNativeFeedback onPress={() => console.log('TouchableNativeFeedback')}>
-        <View style={{
-          width: 200,
-          height: 70,
-          backgroundColor: "dodgerblue"
-        }} />
-      </TouchableNativeFeedback>
+      <Button
+        title="Click Me"
+        color="orange"
+        onPress={() => console.log('Button tapped')} />
     </SafeAreaView>
   );
 }
