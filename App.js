0712b9b2c2ab998c731e41476090eddcb18d249b
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-25 15:55:39 +0530

    add design using flex

--- App.js
+++ App.js
@@ -1,52 +1,29 @@
 import React from 'react';
-import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 import {
-  StyleSheet,
-  SafeAreaView,
   View,
-  Dimensions
 } from 'react-native';
 
 export default function App() {
-  // get device orientations
-  const { landscape, portrait } = useDeviceOrientation();
 
   return (
-    // we can pass multiple objects to style attribute
-    <SafeAreaView style={styles.container}>
+    <View style={{
+      backgroundColor: '#fff',
+      flex: 1
+    }}>
+
       <View style={{
         backgroundColor: 'dodgerblue',
-        width: '100%',
-        height: landscape ? '100%' : '30%'
-      }}></View>
-    </SafeAreaView>
+        flex: 1
+      }} />
+      {/* we can duplicate our codes using Shift+Alt+Down Arrow */}
+      <View style={{
+        backgroundColor: 'gold',
+        flex: 1
+      }} />
+      <View style={{
+        backgroundColor: 'tomato',
+        flex: 1
+      }} />
+    </View>
   );
 }
-// this StyleSheet,create method will validate styles - spelling mistakes
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  },
-});
-
-
-// Density-Independent Pixels
-// Physical Pixels = DIP x Scale Factor
-
-
-/**
- * Model        -> 4, 4S
- * Points       -> 320 x 480
- * Scale Factor -> 2x
- * Pixels       -> 640 x 960
- * View Width   -> 150 x 2 = 300
- */
-
-/**
-* Model        -> 11 Pro Max
-* Points       -> 414 x 896
-* Scale Factor -> 3x
-* Pixels       -> 1242 x 2688
-* View Width   -> 150 x 3 = 450
-*/
