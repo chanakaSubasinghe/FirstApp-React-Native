103142c09821788998973efd641f91b02c5e0248
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-25 23:12:25 +0530

    add flexDirection

--- App.js
+++ App.js
@@ -8,21 +8,25 @@ export default function App() {
   return (
     <View style={{
       backgroundColor: '#fff',
-      flex: 1
+      flex: 1,
+      flexDirection: 'row'
     }}>
 
       <View style={{
         backgroundColor: 'dodgerblue',
-        flex: 1
+        width: 100,
+        height: 100
       }} />
-      {/* we can duplicate our codes using Shift+Alt+Down Arrow */}
+
       <View style={{
         backgroundColor: 'gold',
-        flex: 1
+        width: 100,
+        height: 100
       }} />
       <View style={{
         backgroundColor: 'tomato',
-        flex: 1
+        width: 100,
+        height: 100
       }} />
     </View>
   );
