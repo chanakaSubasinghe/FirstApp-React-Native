6c4168b9c7869d51784afb5f7e4809642be1e885
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-25 13:03:08 +0530

    add Dimension component

--- App.js
+++ App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import {
   StyleSheet,
   SafeAreaView,
-  Button,
-  Platform,
-  StatusBar
+  View,
+  Dimensions
 } from 'react-native';
 
 export default function App() {
-
+  console.log(Dimensions.get('screen'));
   return (
     // we can pass multiple objects to style attribute
-    // style={[styles.container],[containerStyle]}
     <SafeAreaView style={styles.container}>
-
-      <Button
-        title="Click Me"
-        onPress={() => console.log('Yes')} />
-
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: '50%',
+        height: 70
+      }}></View>
     </SafeAreaView>
   );
 }
@@ -25,7 +23,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    backgroundColor: '#fff'
   },
 });
+
+
+// Density-Independent Pixels
+// Physical Pixels = DIP x Scale Factor
+
+
+/**
+ * Model        -> 4, 4S
+ * Points       -> 320 x 480
+ * Scale Factor -> 2x
+ * Pixels       -> 640 x 960
+ * View Width   -> 150 x 2 = 300
+ */
+
+/**
+* Model        -> 11 Pro Max
+* Points       -> 414 x 896
+* Scale Factor -> 3x
+* Pixels       -> 1242 x 2688
+* View Width   -> 150 x 3 = 450
+*/
