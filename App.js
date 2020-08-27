6cd073c6b3259370f543dd006d27125bd658401c
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-27 06:39:56 +0530

    add flexWrap and alignContent

--- App.js
+++ App.js
@@ -11,14 +11,15 @@ export default function App() {
       flex: 1,
       flexDirection: 'row', // horizontal
       justifyContent: 'center', // main or primary axis
-      alignItems: 'center' // secondary axis -> default is stretch
+      alignItems: 'center', // secondary axis -> default is stretch
+      flexWrap: 'wrap', // enabling wrapping - each items get their own width and height
+      alignContent: 'center' // all content will set to center
     }}>
 
       <View style={{
         backgroundColor: 'dodgerblue',
         width: 100,
         height: 100,
-        alignSelf: 'flex-start' // change individual item
       }} />
 
       <View style={{
@@ -28,6 +29,16 @@ export default function App() {
       }} />
       <View style={{
         backgroundColor: 'tomato',
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: 'gray',
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: 'greenyellow',
         width: 100,
         height: 100
       }} />
