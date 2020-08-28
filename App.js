faaaf24311bec14f8a99e6a73e9e7c2e5af1d191
chanakaSubasinghe <chanaka.s@icloud.com>
2020-08-28 09:16:19 +0530

    add flexBasis

--- App.js
+++ App.js
@@ -12,13 +12,12 @@ export default function App() {
       flexDirection: 'row', // horizontal
       justifyContent: 'center', // main or primary axis
       alignItems: 'center', // secondary axis -> default is stretch
-      flexWrap: 'wrap', // enabling wrapping - each items get their own width and height
-      alignContent: 'center' // all content will set to center
     }}>
 
       <View style={{
         backgroundColor: 'dodgerblue',
-        width: 100,
+        flexBasis: 100, // set width or height with primary axis
+        // width: 100,
         height: 100,
       }} />
 
@@ -29,16 +28,6 @@ export default function App() {
       }} />
       <View style={{
         backgroundColor: 'tomato',
-        width: 100,
-        height: 100
-      }} />
-      <View style={{
-        backgroundColor: 'gray',
-        width: 100,
-        height: 100
-      }} />
-      <View style={{
-        backgroundColor: 'greenyellow',
         width: 100,
         height: 100
       }} />
